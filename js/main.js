5a33b6a4e06f361c218ec5acaed879e418ce7528
Narrod-Dev <nerlingfhg@gmail.com>
2025-10-31 20:09:36 -0600

    2. Actualizacion de vistas del home y eliminacion de archivos importantes

--- js/main.js
+++
@@ -1,8 +0,0 @@
-// Importar módulos
-import { initFormHandlers, initFormSwitcher } from './forms.js';
-
-// Inicializar todos los componentes
-document.addEventListener('DOMContentLoaded', () => {
-    initFormHandlers();
-    initFormSwitcher();
-});
